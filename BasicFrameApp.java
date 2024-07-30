0e49f55ef842239c4e330919304dfb74a38a47ed
KayTersy06 <94805095+KayTersy06@users.noreply.github.com>
2024-07-30 13:28:56 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,22 @@
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+/**
+ *
+ * @author kayte
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //declare variables
+        MyFirstFrame myFrame;
+        
+        //instantiate the frame
+        myFrame = new MyFirstFrame();
+    }
+    
+}
